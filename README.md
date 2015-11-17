ea4f0a86512b47e820b0a9c2bf8851fdb2e02316
Brandon Kelly <brandon@pixelandtonic.com>
2015-11-17 05:31:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Big Dates plugin for Craft
+
+This plugin gets Craft to show full timestamps for all Post and Expiry dates on Entry indexes in Craft CMS.
+
+## Installation
+
+To install BigDates, copy the bigdates/ folder into craft/plugins/, and then go to Settings > Plugins and click the “Install” button next to “Big Dates”.
---
+++ bigdates/BigDatesPlugin.php
@@ -0,0 +1,40 @@
+<?php
+namespace Craft;
+
+/**
+ * Big Dates plugin class
+ */
+class BigDatesPlugin extends BasePlugin
+{
+	public function getName()
+	{
+	    return 'Big Dates';
+	}
+
+	public function getVersion()
+	{
+	    return '1.1';
+	}
+
+	public function getDeveloper()
+	{
+	    return 'Pixel & Tonic';
+	}
+
+	public function getDeveloperUrl()
+	{
+	    return 'http://pixelandtonic.com';
+	}
+
+	public function getEntryTableAttributeHtml(BaseElementModel $element, $attribute)
+	{
+		$value = $element->$attribute;
+
+		if ($value instanceof DateTime)
+		{
+			return $value->localeDate().' '.$value->localeTime();
+		}
+
+		return null;
+	}
+}
